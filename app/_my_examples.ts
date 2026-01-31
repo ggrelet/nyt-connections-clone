0644af72923732d12586b5538439622af03390e0
GuillaumeGreletSonos <guillaume.grelet@sonos.com>
2026-01-31 20:49:16 +0100

    Fix name of category

--- app/_my_examples.ts
+++ app/_my_examples.ts
@@ -2,22 +2,22 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: 'Ingredients of a UX Story',
+    category: 'Ingredients of a UX Story'.toUpperCase(),
     items: ["STEP", "VOICE", "ASSISTANT", "ACTION"],
     level: 1,
   },
   {
-    category: "Wind instruments",
+    category: "Wind instruments".toUpperCase(),
     items: ["CLARINET", "FLUTE", "PICCOLO", "FLAGEOLET"],
     level: 2,
   },
   {
-    category: "GROCERY STORE AISLES",
+    category: "Video games with name '_ word'".toUpperCase(),
     items: ["KNIGHT", "FANTASY", "CRUSH", "PRINCE"],
     level: 3,
   },
   {
-    category: "Sports you sometimes practice (one letter substituted)",
+    category: "Sports you sometimes practice (one letter substituted)".toUpperCase(),
     items: ["SQUISH", "RUM", "GRAIL", "VALLEY"],
     level: 4,
   },
