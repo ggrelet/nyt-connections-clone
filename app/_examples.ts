66e7bf6cf59c2da1d0c3a600f538a1f471a9b570
Guillaume GRELET <guillaume.grelet@softbankrobotics.com>
2026-01-31 19:04:26 +0100

    Add gh-pages deployment

--- app/_examples.ts
+++ app/_examples.ts
@@ -2,23 +2,23 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: 'ENCOURAGE, WITH "ON"',
-    items: ["EGG", "GOAD", "SPUR", "URGE"],
+    category: 'Ingredients of a UX Story',
+    items: ["STEP", "VOICE", "ASSISTANT", "ACTION"],
     level: 1,
   },
   {
-    category: "SPHERICAL FOODS",
-    items: ["JAWBREAKER", "MEATBALL", "MOZZARELLA", "ORANGE"],
+    category: "Wind instruments",
+    items: ["CLARINET", "FLUTE", "PICCOLO", "FLAGEOLET"],
     level: 2,
   },
   {
     category: "GROCERY STORE AISLES",
-    items: ["DAIRY", "FROZEN", "PRODUCE", "SNACK"],
+    items: ["KNIGHT", "FANTASY", "CRUSH", "PRINCE"],
     level: 3,
   },
   {
-    category: "GO ___",
-    items: ["BANANAS", "FIGURE", "FISH", "STEADY"],
+    category: "Sports you sometimes practice (one letter substituted)",
+    items: ["SQUISH", "RUM", "GRAIL", "VALLEY"],
     level: 4,
   },
 ];
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    images: {
+        unoptimized: true
+    },
+    basePath: "/nyt-connections-clone",
+    assetPrefix: "/nyt-connections-clone/",
+};
 
 export default nextConfig;
